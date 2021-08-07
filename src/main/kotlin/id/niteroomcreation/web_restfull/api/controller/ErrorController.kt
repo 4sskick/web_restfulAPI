b7b009afe0e33a4c026c4526a06de9682c2199a3
Septian Adi Wijaya <developadi@gmail.com>
2021-08-07 16:30:17 +0700

    application layer - Exception API get product

--- src/main/kotlin/id/niteroomcreation/web_restfull/api/controller/ErrorController.kt
+++ src/main/kotlin/id/niteroomcreation/web_restfull/api/controller/ErrorController.kt
@@ -1,5 +1,6 @@
 package id.niteroomcreation.web_restfull.api.controller
 
+import id.niteroomcreation.web_restfull.api.error.NotFoundException
 import id.niteroomcreation.web_restfull.api.model.WebResponse
 import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.RestControllerAdvice
@@ -9,11 +10,18 @@ import javax.validation.ConstraintViolationException
 class ErrorController {
 
       @ExceptionHandler(value = [ConstraintViolationException::class])
-      fun validationHandler(constraintViolationException: ConstraintViolationException):WebResponse<String>{
+      fun validationHandler(constraintViolationException: ConstraintViolationException): WebResponse<String> {
             return WebResponse(
                     code = 400,
                     status = "BAD REQUEST",
                     data = constraintViolationException.message!!
             )
+      }
+
+      @ExceptionHandler(value = [NotFoundException::class])
+      fun validationHandlerNotFound(notFound: NotFoundException): WebResponse<String> {
+            return WebResponse(code = 404,
+                    status = "NOT FOUND",
+                    data = "Data Not Found")
       }
 }
